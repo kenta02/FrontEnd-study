d3e126d0a1cff718d2303d33b5c18ad68b12d9fb
Kenta Yoshikawa <skycrown02@gmail.com>
2017-05-06 15:32:48 +0900

    second commit

--- vue/vue-facebookLike/app.js
+++ vue/vue-facebookLike/app.js
@@ -1,21 +1,12 @@
 new Vue({
 
-  // // カウントする
-  // el: '#app',
-  // data: {
-  //   count: 0
-  // }
-
+// カウントする
+   el: '#app',
+   data: {
+     count: 0
+   }
 
 
 })
 
 
-//
-// new Vue({
-//   // メッセージ表示
-//   // el:'#app',
-//   // data:{
-//   //   message:'Hello Vue.js'
-//
-// })
