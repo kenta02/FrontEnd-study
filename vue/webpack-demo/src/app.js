cd76b59476036de56a863748a45d05a867b1b912
kenta02 <kenta.yoshikawa.dev@gmail.com>
2017-10-19 00:40:58 +0900

    一部フォルダの削除

--- vue/webpack-demo/src/app.js
+++
@@ -1,9 +0,0 @@
-import {groupBy}from 'lodash/collection'
-import people from './people'
-
-const managerGroups = groupBy(people, 'manager')
-
-const root = document.querySelector("#root")
-root.innerHTML =`<pre>${JSON.stringify(managerGroups,null,2)}</pre>`
-
-// 注：相対パスを使わないで'lodash/collection'のようにすると/node_modulesにインストールしたnpmのモジュールを指すので、自分のモジュールを読み込むのと区別するには常に'./people'のような相対パスを使用してください。
