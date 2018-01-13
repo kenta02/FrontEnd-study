3059f5782153a5dc08696ea6e27cd6ba6e3f5096
kenta02 <kenta.yoshikawa.dev@gmail.com>
2018-01-14 01:49:20 +0900

    不要なファイルの削除

--- my-frontendTemplate/gulp/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-let gulp = require('gulp');
-let requireDir = require('require-dir');
-let browserSync = require("browser-sync").create();
-let runSequence = require('run-sequence'); 
-requireDir('./gulp/tasks', { 'resource': true });
-
--- my-frontendTemplate/gulp/tasks/copy.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let gulp = require("gulp");
-let runSequence = require("run-sequence"); //タスクの順次実行
-let config = require('../config').copy;
-
-gulp.task("copy", ()=> {
-        return gulp.src(["dev/images/**"]).pipe(gulp.dest("release/images"));
-      });
