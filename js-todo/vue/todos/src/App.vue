6a4fa91ae12eb93de07cc22ebec439b26b7814ca
Kenta Yoshikawa <kenta.yoshikawa.dev@gmail.com>
2019-05-20 21:50:15 +0900

    addボタンの追加

---
+++ js-todo/vue/todos/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <form @submit.prevent="add()">
+      <input type="text" v-model="name">
+      <button>Add</button>
+    </form>
+
+    <ul>
+      <li v-for="todo in todos">
+        <!-- <li v-for="(todo index) in todos"> -->
+        {{todo.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      todos: []
+    };
+  },
+  methods: {
+    add() {
+      if (this.name) {
+        // alert(this.name); //画面に出力されるか
+        this.todos = this.todos.concat({ name: this.name });
+        this.name = "";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
